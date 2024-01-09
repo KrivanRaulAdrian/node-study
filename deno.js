b31bd504321ef17698cd4a074790cdc80566ccfc
raulkrivan <krivanrauladrian@gmail.com>
2024-01-09 12:00:20 +0200

    deno

--- deno.js
+++ deno.js
@@ -1,5 +1,6 @@
-function a() {
-  console.log(42);
+const food = Deno.args[0];
+if (food === "love") {
+  console.log("🦕...Deno is born!");
+} else {
+  console.log("🥚...this egg needs some love");
 }
-
-a();
